76deb2386248d3358851bd2f9125e3fb14f89e59
wldsmdl7 <wldmsdl5835@gmail.com>
2026-01-07 17:17:45 +0900

    fix : 회원가입 api 구현

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,13 @@
 import { successHandler } from "../../middlewares/successHandler.js";
 import { userSignUp } from "../services/auth.service.js";
 
-export const handleUserSignUp = async (req, res) => {
-  const { nickname } = req.body;
-  const user = await userSignUp(nickname);
+export const handleUserSignUp = async (req, res, next) => {
+  try {
+    const { nickname } = req.body;
+    const user = await userSignUp(nickname);
 
-  return successHandler(res, "회원가입 성공", { id: user.id });
+    return successHandler(res, "회원가입 성공", { id: user.id });
+  } catch (err) {
+    next(err);
+  }
 };
--- src/index.js
+++ src/index.js
@@ -17,9 +17,6 @@ app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
 
-// 에러 핸들링
-app.use(errorHandler);
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -32,6 +29,9 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Router 연결
 app.use("/", routers);
+
+// 에러 핸들링
+app.use(errorHandler);
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening on port ${port}`);
