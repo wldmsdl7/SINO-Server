968a2fed584e9327c3e083548401296adb7d1428
wldsmdl7 <wldmsdl5835@gmail.com>
2026-01-08 09:50:34 +0900

    chore : cors 설정

--- src/index.js
+++ src/index.js
@@ -12,7 +12,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors()); // cors 방식 허용x
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://3.34.200.244:3000"],
+  })
+);
 app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
